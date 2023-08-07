70786a2ab7ce205a3294c5cf70b964f282a05d43
ralphy <raphael.marandet@gmail.com>
2023-08-07 13:46:32 +0200

    when ranged weapon runs out of ammo : switching to melee

--- src/effects/index.js
+++ src/effects/index.js
@@ -1,4 +1,4 @@
-/* THIS FILE IS AUTO-GENERATED ! DO NOT MODIFY ! Date: 06/08/2023 02:06:56 */
+/* THIS FILE IS AUTO-GENERATED ! DO NOT MODIFY ! Date: 07/08/2023, 13:41:15 */
 const CONSTS = require('../consts')
 module.exports = {
   [CONSTS.EFFECT_ABILITY_BONUS]: require('./ability-bonus'),
--- src/store/creature/getters/isEquippedWithRangedWeapon.js
+++
@@ -1,7 +0,0 @@
-const CONSTS = require("../../../consts");
-
-/**
- * Renvoie true si la creature est equipée d'une arme à distance (mais pas nécessairement sélectionnée)
- * @return {boolean}
- */
-module.exports = (state, getters) => getters.getEquippedItems[CONSTS.EQUIPMENT_SLOT_WEAPON_RANGED] !== null
