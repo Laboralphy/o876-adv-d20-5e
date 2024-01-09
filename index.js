ff92f4d6749c134ee494a9824ea6ae17110a51a0
ralphy <raphael.marandet@gmail.com>
2024-01-09 19:33:17 +0100

    fix CONSTS export

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const Manager = require('./src/Manager')
 const Creature = require('./src/Creature')
 const EffectProcessor = require('./src/EffectProcessor')
+const EntityFactory = require('./src/EntityFactory')
 const AssetManager = require('./src/AssetManager')
 const { Config, CONFIG } = require('./src/config')
-const { CONSTS } = require('./src/consts')
+const CONSTS = require('./src/consts')
+const Evolution = require('./src/Evolution')
 const publicAssets = {
     en: require('./src/public-assets/public-assets.en.json'),
     fr: require('./src/public-assets/public-assets.fr.json')
 }
 
 module.exports = {
-    Manager,
+    AssetManager,
+    CONFIG,
+    CONSTS,
+    Config,
     Creature,
     EffectProcessor,
-    AssetManager,
-    Config,
-    CONFIG,
-    publicAssets,
-    CONSTS
+    EntityFactory,
+    Evolution,
+    Manager,
+    publicAssets
 }
