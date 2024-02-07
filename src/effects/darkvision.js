13c232b3d6f7a65289dcb38a86b6e29beaeb23d5
ralphy <raphael.marandet@gmail.com>
2024-02-07 12:56:16 +0100

    fa

--- src/effects/darkvision.js
+++ src/effects/darkvision.js
@@ -2,7 +2,7 @@ const createEffect = require('./abstract')
 const CONSTS = require('../consts')
 
 /**
- * This effect permit a creature to see in dark areas
+ * This effect permit a creature to see in dark areas.
  * @returns {D20Effect}
  */
 function create () {
